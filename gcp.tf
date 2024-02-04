a13a6fbd49a47a9bc154baa7b1bb81537355333a
Rameshwar Dudhe <141106013+rndudhe1808@users.noreply.github.com>
2024-02-04 11:22:23 +0530

    Update gcp.tf

--- gcp.tf
+++ gcp.tf
@@ -1,34 +1,34 @@
 terraform {
   required_providers {
     google = {
-        source = "harshicorp/google"
-        version = "3.5.0"
+      source  = "hashicorp/google"
+      version = "3.5.0"
     }
   }
 }
-provider "google" {
-    #accidentials = file
-    project = ""
-    region = "us-west1"
-    zone = "us-east1-b"
 
+provider "google" {
+  credentials = file("/root/multi-cloud/gcp/cred.json")  # Specify the path to your Google Cloud credentials file
+  project     = "multi-cloud-413304"  # Replace with your Google Cloud project ID
+  region      = "us-west1"
+  zone        = "us-east1-b"
 }
+
 resource "google_compute_network" "vpc_network" {
-    name = "terraform-network"  
+  name = "terraform-network"
 }
-resource "gooogle_compute_instance" "vm_instance" {
-    name = "terraform-instance"
-    machine_type = "ec2-micro"
-    boot_disk {
-        initialoize_params {
-            image = "debian-cloud/debian-11"
-        }
+
+resource "google_compute_instance" "vm_instance" {
+  name         = "terraform-instance"
+  machine_type = "e2-micro"  # Corrected the machine type
+
+  boot_disk {
+    initialize_params {
+      image = "debian-cloud/debian-11"
     }
-    network_interface {
-        network = google_compute_network.vpc_network.name
-        acess_config {
-            
-        }
-    }
-  
+  }
+
+  network_interface {
+    network = google_compute_network.vpc_network.name
+  }
 }
